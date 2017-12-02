ed3b25e31ecf1469e0a4e2651242591326c7bcce
Tuan Nguyen <nqtuan0192@gmail.com>
2017-12-02 17:21:43 +0700

    Edit file main.cu

--- main.cu
+++ main.cu
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <cuda.h>
 
+#include "helper.h"
+#include "ManagedStruct.h"
+
+// CUDA kernel to add elements of two arrays
+__global__ void add(uint32_t n, float* x, float* y) {
+	uint32_t index = blockIdx.x * blockDim.x + threadIdx.x;
+	uint32_t stride = blockDim.x * gridDim.x;
+	for (int i = index; i < n; i += stride)
+		y[i] = x[i] + y[i];
+}
+
 int main(int argc, char** argv) {
 	/* manually set device */
 	int device_id = 0;
@@ -11,8 +22,34 @@ int main(int argc, char** argv) {
 	cudaSetDevice(device_id);
 	
 	/* start coding from here */
+	uint32_t N = 1 << 20;
+	
+	ManagedStruct ms[2] = {N, N};
+	for (int i = 0; i < 2; ++i) {
+		ms[i].randomize();
+	}
+	
+	//std::cout << ms[0] << std::endl;
+	//std::cout << ms[1] << std::endl;
+	
+	uint32_t blockSize = 256;
+	uint32_t numBlocks = (N + blockSize - 1) / blockSize;
+	
+	add<<<numBlocks, blockSize>>>(N, ms[0]._data, ms[1]._data);
+	
+	cudaDeviceSynchronize();
+	//std::cout << ms[1] << std::endl;
+	
 
 
 	cudaDeviceReset();
 	return 0;
 }
+
+
+/**
+ * https://devblogs.nvidia.com/parallelforall/unified-memory-cuda-beginners/
+ * https://devblogs.nvidia.com/parallelforall/unified-memory-in-cuda-6/
+ * 
+ * 
+ * */
